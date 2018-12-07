5e8015db3523d25ad2fd696675442b0c5d4fcd33
gug <1065803955@qq.com>
2018-12-07 08:13:33 +0800

    10

---
+++ WebBootstart01/WebBootstart01/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBootstart01.Models;
+namespace WebBootstart01.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "你好，MVC";
+        }
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "小马";
+            c.Address = "mmmmmmm";
+            return c;
+        }
+    }
+}
