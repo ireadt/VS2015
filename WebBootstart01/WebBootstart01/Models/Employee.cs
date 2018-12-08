ae2a6ebde8c9b36bc8c10f5e81cd624a52179ccf
gug <1065803955@qq.com>
2018-12-08 12:14:10 +0800

    1

---
+++ WebBootstart01/WebBootstart01/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBootstart01.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public string Client { get; set; }
+        public string From { get; set; }
+    }
+}
