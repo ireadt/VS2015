d81a4519773f43672c58c063b115fb5982dd4cde
gug <1065803955@qq.com>
2018-12-18 18:14:00 +0800

    gg

---
+++ CodeFirstNewDatabaseSample/CodeFirstNewDatabaseSample/BusinessLayer/BlogBusinessLayer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using CodeFirstNewDatabaseSample.Models;
+using CodeFirstNewDatabaseSample.DataAccessLayer;
+
+namespace CodeFirstNewDatabaseSample.BusinessLayer
+{
+    public class BlogBusinessLayer
+    {
+        public void Add(Blog blog)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+            }
+        }
+        public List<Blog>Query()
+        {
+            using (var db = new BloggingContext())
+            {
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                return query.ToList();
+            }
+        }
+        public void Update(Blog blog)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Entry(blog).State = EntityState.Modified;
+
+                db.SaveChanges();
+            }
+        }
+        public Blog Query(int id)
+        {
+            using (var db = new BloggingContext())
+            {
+                return db.Blogs.Find(id);
+            }
+        }
+    }
+}
---
+++ CodeFirstNewDatabaseSample/CodeFirstNewDatabaseSample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstNewDatabaseSample.Models;
+using CodeFirstNewDatabaseSample.BusinessLayer;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateNewBlog();
+            QueryBlog();
+            Updata();
+            QueryBlog();
+            Console.WriteLine("按任意键退出");
+            Console.ReadKey();
+        }
+        static void CreateNewBlog()
+        {
+            Console.Write("请输入一个新博客:");
+            string name = Console.ReadLine();
+            Blog blog = new Blog ();
+            blog.Name = name;
+            BlogBusinessLayer bbl = new BlogBusinessLayer();
+            bbl.Add(blog);
+        }
+        static void QueryBlog()
+        {
+            BlogBusinessLayer bbl = new BlogBusinessLayer();
+            var query = bbl.Query();
+            Console.WriteLine("所有数据库中的博客:");
+            foreach(var item in query)
+            {
+                Console.Write(item.BlogId + "");
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        static void Updata()
+        {
+            BlogBusinessLayer bbl = new BlogBusinessLayer();
+            Console.Write("请输入一个博客id:");
+            int id = int.Parse(Console.ReadLine());
+            Blog blog = bbl.Query(id);
+            Console.Write("请输入新的博客名:");
+            string name = Console.ReadLine();
+            blog.Name = name;
+            bbl.Update(blog);
+        }
+    }
+}
